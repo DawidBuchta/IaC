0dff5af24ea519c4bb530d43279a705355ff9cfb
Dawid Buchta - Windows <dawidbuchta@onet.pl>
2026-01-25 10:54:52 +0100

    - Stworzenie VM linux oraz dodatkowych interfejsów  do tych maszyn - Stworzenie pliku konfiguracyjnego ansible - stworzenie playbookow do ADDS, DNS, SQL oraz www - Wyciągnięcie wrażliwych zmiennych do osobnych plików

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-#Set-Alias -name tf -value terraform
-#tf fmt
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.49.0"
+      version = "4.58.0"
     }
   }
 }
@@ -17,14 +14,15 @@ provider "azurerm" {
   tenant_id       = "50c76291-0c80-4444-a2fb-4f8ab168c311"
 }
 
-
+//["germanywestcentral","swedencentral","italynorth","uksouth","norwayeast"]
 resource "azurerm_resource_group" "IaC" {
 
   name     = "Infrastructure_as_Code"
-  location = "norwayeast"
+  location = "uksouth"
 
 }
 
+//siec główna
 resource "azurerm_virtual_network" "Azure-VNet" {
   name                = "IaC-network"
   location            = azurerm_resource_group.IaC.location
@@ -34,6 +32,7 @@ resource "azurerm_virtual_network" "Azure-VNet" {
 
 }
 
+//sieć lokalna
 resource "azurerm_subnet" "LAN" {
 
   name                 = "IaC-LAN"
@@ -43,6 +42,7 @@ resource "azurerm_subnet" "LAN" {
   depends_on           = [azurerm_virtual_network.Azure-VNet, azurerm_resource_group.IaC]
 }
 
+//grupa zabezpieczen
 resource "azurerm_network_security_group" "Sec-Grp" {
 
   name                = "IaC-SecurityGroup"
@@ -55,6 +55,7 @@ resource "azurerm_network_security_group" "Sec-Grp" {
   }
 }
 
+//firewall rule
 resource "azurerm_network_security_rule" "Inbound-Allow-ALL" {
   name                        = "Inbound-Allow-ALL"
   priority                    = 100
@@ -70,27 +71,43 @@ resource "azurerm_network_security_rule" "Inbound-Allow-ALL" {
   depends_on                  = [azurerm_network_security_group.Sec-Grp, azurerm_resource_group.IaC]
 }
 
+//przypisanie podsieci do d ogrupy zabezpieczen
 resource "azurerm_subnet_network_security_group_association" "SEc-Grp-Assosiation" {
   subnet_id                 = azurerm_subnet.LAN.id
   network_security_group_id = azurerm_network_security_group.Sec-Grp.id
   depends_on                = [azurerm_subnet.LAN, azurerm_resource_group.IaC, azurerm_network_security_group.Sec-Grp]
 }
 
-resource "azurerm_public_ip" "public-ip" {
-  name                    = "public-ip"
+//ip zewnetrzne do vm main
+resource "azurerm_public_ip" "wan1" {
+  name                    = "wan1"
   location                = azurerm_resource_group.IaC.location
   resource_group_name     = azurerm_resource_group.IaC.name
   allocation_method       = "Static"
   idle_timeout_in_minutes = 30
-  depends_on          = [azurerm_resource_group.IaC]
+  depends_on              = [azurerm_resource_group.IaC]
+
+  tags = {
+    environment = "Dev"
+  }
+}
+//ip zewnetrzne do vm linux
+resource "azurerm_public_ip" "wan2" {
+  name                    = "wan2"
+  location                = azurerm_resource_group.IaC.location
+  resource_group_name     = azurerm_resource_group.IaC.name
+  allocation_method       = "Static"
+  idle_timeout_in_minutes = 30
+  depends_on              = [azurerm_resource_group.IaC]
 
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_network_interface" "Interface" {
-  name                = "Interface"
+//interfejs sieciowy podpinany do vm main
+resource "azurerm_network_interface" "lancard1" {
+  name                = "lancard1"
   location            = azurerm_resource_group.IaC.location
   resource_group_name = azurerm_resource_group.IaC.name
   depends_on          = [azurerm_resource_group.IaC]
@@ -98,8 +115,28 @@ resource "azurerm_network_interface" "Interface" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.LAN.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public-ip.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "172.22.1.10"
+    public_ip_address_id          = azurerm_public_ip.wan1.id
+  }
+  tags = {
+
+    envoriment = "dev"
+  }
+}
+//interfejs sieciowy podpinany do vm linux
+resource "azurerm_network_interface" "lancard2" {
+  name                = "lancard2"
+  location            = azurerm_resource_group.IaC.location
+  resource_group_name = azurerm_resource_group.IaC.name
+  depends_on          = [azurerm_resource_group.IaC]
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.LAN.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "172.22.1.11"
+    public_ip_address_id          = azurerm_public_ip.wan2.id
   }
   tags = {
 
@@ -107,22 +144,23 @@ resource "azurerm_network_interface" "Interface" {
   }
 }
 
+
 resource "azurerm_windows_virtual_machine" "main-server" {
   name                = "Main"
   resource_group_name = azurerm_resource_group.IaC.name
   location            = azurerm_resource_group.IaC.location
-  size                = "Standard_B2as_v2"
+  size                = "Standard_B2as_v2" 
   admin_username      = var.admin_username
   admin_password      = var.admin_password
-  depends_on          = [azurerm_resource_group.IaC,azurerm_network_interface.Interface]
+  depends_on          = [azurerm_resource_group.IaC, azurerm_network_interface.lancard1]
 
   network_interface_ids = [
-    azurerm_network_interface.Interface.id
+    azurerm_network_interface.lancard1.id
   ]
 
-  priority                  = "Spot"
-  eviction_policy           = "Deallocate"
-  automatic_updates_enabled = "false"
+  //priority                  = "Spot"
+  //eviction_policy           = "Deallocate"
+  automatic_updates_enabled = false
   computer_name             = "Tatake"
   patch_mode                = "AutomaticByPlatform"
 
@@ -139,5 +177,62 @@ resource "azurerm_windows_virtual_machine" "main-server" {
     sku       = "2025-datacenter-azure-edition-smalldisk"
     version   = "latest"
   }
+}
+resource "azurerm_virtual_machine_extension" "enable_winrm_main" {
+  name                 = "enable-winrm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.main-server.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = jsonencode({
+    commandToExecute = "powershell -ExecutionPolicy Bypass -Command \"Enable-PSRemoting -Force; winrm quickconfig -q; winrm set winrm/config/service '@{AllowUnencrypted=\\\"true\\\"}'; winrm set winrm/config/service/auth '@{Basic=\\\"true\\\"}'; New-NetFirewallRule -DisplayName 'WinRM-HTTP-5985' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 5985 -Profile Any\""
+  })
+}
+
+resource "azurerm_linux_virtual_machine" "linux-server" {
+  name                            = "linux"
+  resource_group_name             = azurerm_resource_group.IaC.name
+  location                        = azurerm_resource_group.IaC.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  depends_on                      = [azurerm_resource_group.IaC, azurerm_network_interface.lancard2]
+  network_interface_ids = [
+    azurerm_network_interface.lancard2.id
+  ]
+
+  //priority        = "Spot"
+  //eviction_policy = "Deallocate"
+  computer_name         = "Tatake"
+  patch_mode            = "ImageDefault"
+  patch_assessment_mode = "ImageDefault"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+
+  }
+
+
+  source_image_reference {
+    publisher = "debian"
+    offer     = "debian-13"
+    sku       = "13"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-CLOUDINIT
+#cloud-config
+package_update: true
+packages:
+  - ansible
+  - python3-winrm
+  - sshpass
+runcmd:
+  - ansible-galaxy collection install ansible.windows community.windows microsoft.ad chocolatey.chocolatey
+CLOUDINIT
+  )
 }
 
